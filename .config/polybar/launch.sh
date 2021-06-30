abbab626f59fd44c4c47c88f27786a6405c5acce
sryeturu <sandeep.r.yeturu@gmail.com>
2021-06-29 19:26:36 -0500

    rofi stuff

--- .config/polybar/launch.sh
+++
@@ -1,8 +0,0 @@
-# Terminate already running bar instances
-killall -q polybar
-
-# killall doesn't hang, so we wait until instance is killed
-while pgrep -x polybar >/dev/null; do sleep 1;done
-
-# Launch bar
-polybar bar
