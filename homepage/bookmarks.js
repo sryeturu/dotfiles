31913cf658f6f3c2f5ca36651e234b74305904e6
sryeturu <sandeep@peachpay.app>
2023-01-21 03:57:11 -0600

    upd

--- homepage/bookmarks.js
+++ homepage/bookmarks.js
@@ -1,28 +1,19 @@
 // startup homepage from: https://github.com/Jaredk3nt/homepage
 
-// Note: having length != 4 will mess with layout based on how the site is styled
 const bookmarks = [
   {
     title: ".",
     links: [
       { name: "Youtube", url: "https://youtube.com" },
       { name: "Reddit", url: "https://reddit.com"},
-    ],
-  },
-
-  {
-    title: "..",
-    links: [
-      { name: "Solar Movies", url: "https://www1.solarmovies.movie/" },
-      { name: "Sports Bay", url: "https://sportsbay.org/" },
-      { name: "Netflix", url: "https://www.netflix.com/browse"},
+      {name: "Netflix", url: "https://www.netflix.com/browse"},
     ],
   },
 
   {
     title: "...",
     links: [
-      { name: "Gmail", url: "https://inbox.google.com" },
+      { name: "Email", url: "https://inbox.google.com" },
       { name: "GitHub", url: "https://github.com/sryeturu?tab=repositories"},
     ],
   },
